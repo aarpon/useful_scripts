b1473886a75921f06c76bd4b73e3955133d47432
Aaron Ponti <aarpon@gmail.com>
2021-08-24 20:13:50 +0200

    Add compression.

--- resources/build_exe.cmd
+++ resources/build_exe.cmd
@@ -1,2 +1,2 @@
 REM To build, use the following command
-python.exe -m nuitka --assume-yes-for-downloads --standalone --onefile --mingw64 ..\useful_scripts\cleanDirTree.py --output-dir=..\dist
+python.exe -m nuitka --assume-yes-for-downloads --onefile --mingw64 ..\useful_scripts\cleanDirTree.py --output-dir=..\dist
